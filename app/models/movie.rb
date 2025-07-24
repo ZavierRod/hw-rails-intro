d3d2b36e2486db34d0d991eac1b78f1dd982b4e4
Zavier Rodrigues <zavier_rod@berkeley.edu>
2025-07-24 17:45:57 +0000

    Finished part 1 I think

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ApplicationRecord
+  RATINGS = ['G','PG','PG-13','R']
+  def self.all_ratings
+    return RATINGS
+  end
+
+  def self.with_ratings(list_of_ratings)
+    if list_of_ratings.nil? || list_of_ratings.empty?
+      return Movie.all
+    else
+      filtered_list = list_of_ratings & Movie.all_ratings
+      Movie.where(rating: filtered_list)
+    end
+  end
 end
